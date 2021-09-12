d4ad04164ebc3f25c0d5d06d5bc649e405d0ee29
kelvins-io <mybaishati@gmail.com>
2021-09-12 15:59:06 +0800

    若干更新

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"gitee.com/cristiane/micro-mall-sku-cron/startup"
-	"gitee.com/cristiane/micro-mall-sku-cron/vars"
 	"gitee.com/kelvins-io/kelvins"
 	"gitee.com/kelvins-io/kelvins/app"
 )
@@ -10,7 +9,6 @@ import (
 const APP_NAME = "micro-mall-sku-cron"
 
 func main() {
-	vars.AppName = APP_NAME
 	application := &kelvins.CronApplication{
 		Application: &kelvins.Application{
 			LoadConfig: startup.LoadConfig,
